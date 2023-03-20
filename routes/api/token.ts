ffc272120bdcd4d68c696a1c2cc862f6713e46e3
Glan_Sheep <kopkcgungun@gmail.com>
2023-03-20 09:10:11 +0900

    Update token.ts

--- routes/api/token.ts
+++ routes/api/token.ts
@@ -1,8 +1,23 @@
 import { Handlers } from "$fresh/server.ts";
+import { getCookies } from "std/http/cookie.ts";
+
+import db from "utils/mongodb.ts"
+
+import type { UserCookieType, VerifyDataType } from "types/db.ts";
+
+const UserCookie = db.collection<UserCookieType>("UserCookie");
+const Verify = db.collection<VerifyDataType>("Verify");
+
 export const handler: Handlers = {
   async GET(req, ctx) {
     console.log(req);
     console.log(ctx);
+    const cookie = getCookies(req.headers);
+    if (cookie["remember-me"]) {
+      const UserId = await UserCookie.findOne({ token: cookie["remember-me"] });
+      if (UserId) {
+      }
+    }
     return new Response("HELLO");
   }
 }
