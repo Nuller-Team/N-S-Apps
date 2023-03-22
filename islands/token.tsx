75fd94c15bad635816adde0130cb02eb53b67b66
Glan_Sheep <kopkcgungun@gmail.com>
2023-03-22 19:22:55 +0900

    TOKEN island完成

--- islands/token.tsx
+++ islands/token.tsx
@@ -7,36 +7,48 @@ interface CounterProps {
 export default function TOKEN() {
   const [name, setName] = useState("");
   const [error, setError] = useState("");
+  const [success, setSuccess] = useState("");
   const fetchToken = async () => {
     console.log(name);
     if (name == "") {
       setError("ERROR：ハンドルネームが空白です！");
-    }else{
-      setError("");
-      const res = await fetch(`api/token?name=${name}`);
+      return;
     }
+    setError("");
+    const res = await fetch(`api/token?name=${name}`);
+    res.json().catch((e) => {
+      setError("ERROR：予期せぬエラーが発生しました");
+    }).then((data) => {
+      if (data["status"] == "Error") {
+        setError(data["text"]);
+      } else {
+        setError("");
+        setSuccess(`http://localhost:8000/verify/${data["text"]}`)
+      }
+    });
   };
   return (
     <div>
       <div class="p-1">
-      <input
-        type="text"
-        id="name"
-        class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-        value={name}
-        onChange={e => setName((e.target as HTMLInputElement).value)}
-        placeholder="handle name"
-      >
-      </input>
+        <input
+          type="text"
+          id="name"
+          class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+          value={name}
+          onChange={(e) => setName((e.target as HTMLInputElement).value)}
+          placeholder="handle name"
+        >
+        </input>
       </div>
       <div class="p-0.5"></div>
       <button
         onClick={fetchToken}
         class="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded-md focus:outline-none focus:ring w-16 flex items-start justify-center"
-        >
+      >
         <p>作成</p>
       </button>
       <p class="text-red-400 font-bold">{error}</p>
+      <p class="text-green-400 font-bold">{success}</p>
     </div>
   );
 }
