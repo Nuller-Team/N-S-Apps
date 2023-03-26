1e44458d9918c53090aa5c9f74d529feb6d29998
Glan_Sheep <kopkcgungun@gmail.com>
2023-03-26 15:08:38 +0900

    タイトル名を変更可能に

--- components/title.tsx
+++ components/title.tsx
@@ -1,12 +1,13 @@
 import type { ComponentChildren } from "preact";
 type TitleType = {
   children: ComponentChildren;
+  name: string;
 };
 
 export default function Title(props: TitleType) {
   return (
       <div class="flex flex-col items-center justify-center h-screen bg-gray-100">
-        <h1 class="text-3xl font-bold text-gray-800 mb-8">N/S CAPTCHA</h1>
+        <h1 class="text-3xl font-bold text-gray-800 mb-8">{props.name}</h1>
         {props.children}
       </div>
   );
