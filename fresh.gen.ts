cf945fcbea62f9727a77a211edcff4b564993482
Glan_Sheep <kopkcgungun@gmail.com>
2023-04-16 10:15:01 +0900

    Update fresh.gen.ts

--- fresh.gen.ts
+++ fresh.gen.ts
@@ -4,34 +4,34 @@
 
 import config from "./deno.json" assert { type: "json" };
 import * as $0 from "./routes/_404.tsx";
-import * as $1 from "./routes/_middleware.ts";
-import * as $2 from "./routes/api/token.ts";
-import * as $3 from "./routes/checker.tsx";
-import * as $4 from "./routes/entry.tsx";
-import * as $5 from "./routes/index.tsx";
-import * as $6 from "./routes/login/callback.tsx";
-import * as $7 from "./routes/result.tsx";
-import * as $8 from "./routes/verify/[token].tsx";
+import * as $1 from "./routes/_app.tsx";
+import * as $2 from "./routes/_middleware.ts";
+import * as $3 from "./routes/api/token.ts";
+import * as $4 from "./routes/checker.tsx";
+import * as $5 from "./routes/entry.tsx";
+import * as $6 from "./routes/index.tsx";
+import * as $7 from "./routes/login/callback.tsx";
+import * as $8 from "./routes/result.tsx";
+import * as $9 from "./routes/verify/[token].tsx";
 import * as $$0 from "./islands/entry.tsx";
-import * as $$1 from "./islands/profile.tsx";
-import * as $$2 from "./islands/token.tsx";
+import * as $$1 from "./islands/token.tsx";
 
 const manifest = {
   routes: {
     "./routes/_404.tsx": $0,
-    "./routes/_middleware.ts": $1,
-    "./routes/api/token.ts": $2,
-    "./routes/checker.tsx": $3,
-    "./routes/entry.tsx": $4,
-    "./routes/index.tsx": $5,
-    "./routes/login/callback.tsx": $6,
-    "./routes/result.tsx": $7,
-    "./routes/verify/[token].tsx": $8,
+    "./routes/_app.tsx": $1,
+    "./routes/_middleware.ts": $2,
+    "./routes/api/token.ts": $3,
+    "./routes/checker.tsx": $4,
+    "./routes/entry.tsx": $5,
+    "./routes/index.tsx": $6,
+    "./routes/login/callback.tsx": $7,
+    "./routes/result.tsx": $8,
+    "./routes/verify/[token].tsx": $9,
   },
   islands: {
     "./islands/entry.tsx": $$0,
-    "./islands/profile.tsx": $$1,
-    "./islands/token.tsx": $$2,
+    "./islands/token.tsx": $$1,
   },
   baseUrl: import.meta.url,
   config,
