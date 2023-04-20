8f3829d6742982bb0420e2162ac342f99b46fb11
Glan_Sheep <kopkcgungun@gmail.com>
2023-04-20 13:59:20 +0900

    バグ修正4

--- islands/times.tsx
+++ islands/times.tsx
@@ -1,7 +1,11 @@
 import { useState, useEffect } from "preact/hooks";
-import { h } from "preact";
 import { State } from "../types/session.ts";
-import { time as date } from "https://deno.land/x/time.ts@v2.0.1/mod.ts";
+import {
+  DateTime,
+  datetime,
+  diffInMillisec,
+} from "https://deno.land/x/ptera@v1.0.2/mod.ts";
+import { h } from "preact";
 
 interface propsType {
   state: State;
@@ -16,74 +20,55 @@ const options = [
 
 export default function TIMES({ state }: propsType) {
   const [haveUsed, setHaveUsed] = useState<boolean>(false);
+  const [EnrollmentDate, setEnrollmentDate] = useState<DateTime>(() => {
+    if (state.admission_month) {
+      let Enrollment_year = 2000 + 20 + state.gen;
+          if (state.school == "N") {
+            Enrollment_year = 2000 + 15 + state.gen;
+          }
+          setHaveUsed(true);
+          if (state.admission_month == "4")
+          return(
+              datetime(`${Enrollment_year}-04-01 00:00`).toZonedTime(
+                "asia/Tokyo"
+              )
+            );
+          if (state.admission_month == "7")
+          return(
+              datetime(`${Enrollment_year}-07-01 00:00`).toZonedTime(
+                "asia/Tokyo"
+              )
+            );
+          if (state.admission_month == "10")
+          return(
+              datetime(`${Enrollment_year}-10-01 00:00`).toZonedTime(
+                "asia/Tokyo"
+              )
+            );
+          if (state.admission_month == "1")
+            return(
+              datetime(`${Enrollment_year + 1}-01-01 00:00`).toZonedTime(
+                "asia/Tokyo"
+              )
+            );
+    }
+    return datetime().toZonedTime("asia/Tokyo")
+  });
   const [selectedOption, setSelectedOption] = useState(options[0]);
   const [error, setError] = useState("");
-  const [EnrollmentDate, setEnrollmentDate] = useState<Date>(
-    date().tz("asia/Tokyo").t
+  const [time, setTime] = useState(
+    diffInMillisec(datetime().toZonedTime("asia/Tokyo"), EnrollmentDate)
   );
-  useEffect(() => {
-    if (state.admission_month) {
-      if (state.admission_month == "4") setSelectedOption(options[0]);
-      if (state.admission_month == "7") setSelectedOption(options[1]);
-      if (state.admission_month == "10") setSelectedOption(options[2]);
-      if (state.admission_month == "1") setSelectedOption(options[3]);
-      setHaveUsed(true);
-    }
-  }, []);
 
-  useEffect(() => {
-    let Enrollment_year = 2000 + 20 + state.gen;
-    if (state.school == "N") {
-      Enrollment_year = 2000 + 15 + state.gen;
-    }
-    if (selectedOption.value == 4) {
-      setEnrollmentDate(
-        date(`${Enrollment_year}-04-01 00:00`).tz("asia/Tokyo").t
-      );
-    } else if (selectedOption.value == 7)
-      setEnrollmentDate(
-        date(`${Enrollment_year}-07-01 00:00`).tz("asia/Tokyo").t
-      );
-    else if (selectedOption.value == 10)
-      setEnrollmentDate(
-        date(`${Enrollment_year}-10-01 00:00`).tz("asia/Tokyo").t
-      );
-    else if (selectedOption.value == 1)
-      setEnrollmentDate(
-        date(`${Enrollment_year + 1}-01-01 00:00`).tz("asia/Tokyo").t
-      );
-  }, []);
-
-  useEffect(() => {
-    let Enrollment_year = 2000 + 20 + state.gen;
-    if (state.school == "N") {
-      Enrollment_year = 2000 + 15 + state.gen;
-    }
-    if (selectedOption.value == 4) {
-      setEnrollmentDate(
-        date(`${Enrollment_year}-04-01 00:00`).tz("asia/Tokyo").t
-      );
-    } else if (selectedOption.value == 7)
-      setEnrollmentDate(
-        date(`${Enrollment_year}-07-01 00:00`).tz("asia/Tokyo").t
-      );
-    else if (selectedOption.value == 10)
-      setEnrollmentDate(
-        date(`${Enrollment_year}-10-01 00:00`).tz("asia/Tokyo").t
-      );
-    else if (selectedOption.value == 1)
-      setEnrollmentDate(
-        date(`${Enrollment_year + 1}-01-01 00:00`).tz("asia/Tokyo").t
-      );
-  }, [selectedOption.value]);
   useEffect(() => {
     const intervalID = setInterval(() => tick(), 1000);
     return () => clearInterval(intervalID);
-  }, [selectedOption.value, haveUsed]);
+  }, [EnrollmentDate]);
 
-  const [time, setTime] = useState(new Date(daysBetween(EnrollmentDate)));
   const tick = () => {
-    setTime(new Date(daysBetween(EnrollmentDate)));
+    const today = datetime().toZonedTime("asia/Tokyo");
+    const times = diffInMillisec(today, EnrollmentDate);
+    setTime(times);
   };
 
   const handleChange = (
@@ -100,7 +85,6 @@ export default function TIMES({ state }: propsType) {
     const res = await fetch(
       `/api/admission_month?month=${selectedOption.value}`
     );
-    console.log(selectedOption.value);
     res
       .json()
       .catch((e) => {
@@ -110,16 +94,38 @@ export default function TIMES({ state }: propsType) {
         if (data["status"] == "Error") {
           setError(data["text"]);
         } else {
-          setHaveUsed(true);
+          let Enrollment_year = 2000 + 20 + state.gen;
+          if (state.school == "N") {
+            Enrollment_year = 2000 + 15 + state.gen;
+          }
+          if (selectedOption.value == 4)
+            setEnrollmentDate(
+              datetime(`${Enrollment_year}-04-01 00:00`).toZonedTime(
+                "asia/Tokyo"
+              )
+            );
+          if (selectedOption.value == 7)
+            setEnrollmentDate(
+              datetime(`${Enrollment_year}-07-01 00:00`).toZonedTime(
+                "asia/Tokyo"
+              )
+            );
+          if (selectedOption.value == 10)
+            setEnrollmentDate(
+              datetime(`${Enrollment_year}-10-01 00:00`).toZonedTime(
+                "asia/Tokyo"
+              )
+            );
+          if (selectedOption.value == 1)
+            setEnrollmentDate(
+              datetime(`${Enrollment_year + 1}-01-01 00:00`).toZonedTime(
+                "asia/Tokyo"
+              )
+            );
         }
       });
+    setHaveUsed(true);
   };
-
-  function daysBetween(startDate: Date) {
-    const today = date().tz("asia/Tokyo").t;
-    const diff = today.getTime() - startDate.getTime();
-    return diff;
-  }
 
   return (
     <>
@@ -132,14 +138,14 @@ export default function TIMES({ state }: propsType) {
               setHaveUsed(false);
             }}
           >
-            {Math.floor(time.getTime() / 1000)}
+            {Math.floor(time / 1000)}
           </a>
           <br></br>
           秒が経過しています
         </div>
       </div>
-      <div hidden={haveUsed}>
-        <div class="flex flex-col items-center justify-center h-screen w-screen bg-gray-100">
+      <div hidden={haveUsed} class="w-full">
+        <div class="flex flex-col items-center justify-center h-screen  bg-gray-100">
           <label
             for="dropdown"
             class="text-xl md:text-2xl lg:text-3xl font-bold mb-2 py-2"
