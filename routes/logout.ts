ce08d698b1a4cc831de7ae418d4219b661cefc20
Glan_Sheep <kopkcgungun@gmail.com>
2023-05-14 21:40:44 +0900

    Cookieの削除を追加

--- routes/logout.ts
+++ routes/logout.ts
@@ -1,4 +1,7 @@
 import type { Handlers } from "$fresh/server.ts";
+import {
+  deleteCookie,
+} from "https://deno.land/std@0.184.0/http/cookie.ts";
 import type { State } from "./_middleware.ts";
 
 // deno-lint-ignore no-explicit-any
@@ -7,6 +10,13 @@ export const handler: Handlers<any, State> = {
     const { error } = await ctx.state.supabaseClient.auth.signOut();
     if (error) throw error;
 
-    return new Response(null, { headers: { location: "/" }, status: 302 });
+    const response = new Response(null, {
+      headers: { location: "/" },
+      status: 303,
+    });
+    deleteCookie(response.headers, "supabase-access-token", {path: "/"})
+
+    console.log(response);
+    return response;
   },
 };
