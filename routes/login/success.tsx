2cf643420c29a11987ff228d6cdb810195e847ce
Glan_Sheep <kopkcgungun@gmail.com>
2023-05-14 18:30:11 +0900

    env管理を変更

--- routes/login/success.tsx
+++ routes/login/success.tsx
@@ -1,11 +1,12 @@
 import type { PageProps } from "$fresh/server.ts";
 import AuthFragmentCatcher from "@/islands/AuthFragmentCatcher.tsx";
+import env from "../../utils/env.ts";
 
 export default function OAuthSuccessPage(props: PageProps) {
   return (
     <AuthFragmentCatcher
-      supabaseUrl={Deno.env.get("SUPABASE_URL")!}
-      supabaseKey={Deno.env.get("SUPABASE_ANON_KEY")!}
+      supabaseUrl={env("SUPABASE_URL")}
+      supabaseKey={env("SUPABASE_ANON_KEY")}
       redirectTo={props.url.searchParams.get("redirect_to") || undefined}
     />
   );
--- utils/supabase.ts
+++ utils/supabase.ts
@@ -3,27 +3,25 @@ import { createClient } from "@supabase/supabase-js";
 import { createServerSupabaseClient } from "@supabase/auth-helpers-shared";
 import { Database } from "./supabase_types.ts";
 import { getCookies, setCookie } from "std/http/cookie.ts";
+import env from "./env.ts";
 
-const SUPABASE_URL = Deno.env.get("SUPABASE_URL")!;
-const SUPABASE_ANON_KEY = Deno.env.get("SUPABASE_ANON_KEY")!;
+const SUPABASE_URL = env("SUPABASE_URL");
+const SUPABASE_ANON_KEY = env("SUPABASE_ANON_KEY");
 
-export const supabase = createClient<Database>(
-	SUPABASE_URL,
-	SUPABASE_ANON_KEY,
-);
+export const supabase = createClient<Database>(SUPABASE_URL, SUPABASE_ANON_KEY);
 
 export const supabaseAdminClient = createClient<Database>(
-  Deno.env.get("SUPABASE_URL")!,
-  Deno.env.get("SUPABASE_SERVICE_ROLE_KEY")!,
+  env("SUPABASE_URL")!,
+  env("SUPABASE_SERVICE_ROLE_KEY")!
 );
 
 export function createSupabaseClient(
   requestHeaders: Headers,
-  responseHeaders?: Headers,
+  responseHeaders?: Headers
 ) {
-	return createServerSupabaseClient({
-    supabaseUrl: Deno.env.get("SUPABASE_URL")!,
-    supabaseKey: Deno.env.get("SUPABASE_ANON_KEY")!,
+  return createServerSupabaseClient({
+    supabaseUrl: env("SUPABASE_URL"),
+    supabaseKey: env("SUPABASE_ANON_KEY"),
     getRequestHeader: (key) => requestHeaders.get(key) ?? undefined,
     getCookie: (name) => {
       const cookie = getCookies(requestHeaders)[name] ?? "";
