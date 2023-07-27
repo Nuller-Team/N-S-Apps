bc1a36dd604796a66fb8c7de609d2faad905fb2a
Glan_Sheep <kopkcgungun@gmail.com>
2023-07-27 16:30:10 +0900

    バグ修正
    リダイレクトURLをenvから設定できるように変更

--- utils/oauth2_client.ts
+++ utils/oauth2_client.ts
@@ -1,7 +1,7 @@
 import { createGoogleOAuth2Client } from "kv_oauth";
 
 export const oauth2Client = createGoogleOAuth2Client({
-  redirectUri: "http://localhost:8000/callback",
+  redirectUri: `${Deno.env.get("REDIRECT_URI")}/callback`,
   defaults: {
     scope: [
       "https://www.googleapis.com/auth/userinfo.profile",
