825e771fdb83cef4bd4730f74ebb6105513598b9
Ehor <123355005+Ehor-developer@users.noreply.github.com>
2023-08-27 00:38:25 +0900

    リダイレクト処理の追加

--- routes/entry.tsx
+++ routes/entry.tsx
@@ -1,84 +1,13 @@
-import { PageProps } from "$fresh/server.ts";
-import { asset } from "$fresh/runtime.ts";
-import EntryForm from "@/islands/entry.tsx";
-import { State } from "@/routes/_middleware.ts";
-import Head from "@/components/Head.tsx";
-import Layout from "@/components/Layout.tsx";
-import { Handlers } from "@/utils/handler.ts";
+import { Handlers } from "$fresh/server.ts";
 
 export const handler: Handlers = {
-  GET(_req, ctx) {
-    return ctx.render({ ...ctx.state });
-  },
-};
-
-const TITLE = "Nullerに応募する";
-const DESCRIPTION = `Nullerは、Discord上で活動する学生による開発チームで、「学生が描く未来」というテーマで、
-便利なアプリや面白いゲーム、今までにないツールを作ることに取り組んでいます。
-チームメンバーは多様であり、コミュニケーションや協働性の確保にも取り組んでいます。
-Nullerの目標は、プログラミングを通じた自己表現や社会貢献です。興味がある方は是非参加してみてください。`;
-
-export default function Entry(props: PageProps<State>) {
-  const ogImageUrl = new URL(asset("/ns-app/entry.png"), props.url).href;
-  if (!props.data.user?.id) {
-    return (
-      <>
-        <Head
-          title={TITLE}
-          description={DESCRIPTION}
-          href={props.url.href}
-          imageUrl={ogImageUrl}
-        />
-        <Layout state={props.data}>
-          <section class="bg-white py-12">
-            <div class="container mx-auto px-4">
-              <div
-                class={
-                  "text-2xl sm:text-4xl md:text-5xl lg:text-6xl 2xl:text-7xl font-semibold mb-8 text-center py-20 md:py-36 space-y-2"
-                }
-              >
-                <h1 class={"text-black"}>Nullerに応募する</h1>
-                <a
-                  href={"/login"}
-                  class="bg-blue-500 hover:bg-blue-700 text-white font-black py-2 px-4 rounded text-2xl"
-                >
-                  ログイン
-                </a>
-              </div>
-              <footer class={"flex justify-center py-10"}>
-                <image src="/svg/entry.svg" />
-              </footer>
-            </div>
-            <div class="flex justify-center">
-              <img
-                class="p-2 rounded-lg shadow-lg w-auto sm:w-96"
-                src={"/ns-app/entry.png"}
-              ></img>
-            </div>
-          </section>
-        </Layout>
-      </>
-    );
-  } else {
-    return (
-      <>
-        <Head
-          title={TITLE}
-          description={DESCRIPTION}
-          href={props.url.href}
-          imageUrl={ogImageUrl}
-        />
-        <Layout state={props.data}>
-          <div class="flex flex-col items-center justify-center h-screen bg-gray-100">
-            <h1 class="text-3xl font-bold text-gray-800 mb-8">
-              Nullerに応募する
-            </h1>
-            <div class="bg-white shadow-md rounded-md p-8 w-full sm:w-[30rem]">
-              <EntryForm state={props.data} />
-            </div>
-          </div>
-        </Layout>
-      </>
-    );
+  GET() {
+    const response = new Response("", {
+      status: 303,
+      headers: {
+        Location: "https://form.nuller.net/entry",
+    },
+  });
+  return response;
   }
 }
