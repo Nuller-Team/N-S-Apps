4b7ba73c1b4500edd9e39bc2e985ac8ee8ac9789
Ehor. <123355005+Ehor-developer@users.noreply.github.com>
2025-10-25 15:14:52 +0900

    著作権表記の年を2025年に更新

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -99,7 +99,7 @@ function Footer(): JSX.Element {
           <hr class="my-6 border-gray-200 sm:mx-auto  lg:my-8" />
           <div class="sm:flex sm:items-center sm:justify-between">
             <span class="text-sm text-gray-500 sm:text-center">
-              © 2023{" "}
+              © 2025{" "}
               <a href="https://nuller.jp/" class="hover:underline">
                 Nuller
               </a>
