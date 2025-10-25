a579dcc88ef32501255da17ab176631d473f957b
Ehor. <123355005+Ehor-developer@users.noreply.github.com>
2025-10-25 18:15:52 +0900

    Alert.tsxをリンクなしでも動作するように

--- components/Alert.tsx
+++ components/Alert.tsx
@@ -1,6 +1,6 @@
 type AlertProps = {
   message: string;
-  link: string;
+  link?: string;
 };
 export default function Alert({ message, link }: AlertProps) {
   return (
@@ -9,12 +9,14 @@ export default function Alert({ message, link }: AlertProps) {
     >
       <div class="m-auto flex md:flex-row flex-col gap-x-4 gap-y-2 justify-center items-center p-2">
         <div>{message}</div>
-        <a
-          class="hover:text-red-700 hover:bg-white py-1 px-4 border-white border-2 rounded-[3px] transition"
-          href={link}
-        >
-          詳細はこちら
-        </a>
+        {link && (
+          <a
+            class="hover:text-red-700 hover:bg-white py-1 px-4 border-white border-2 rounded-[3px] transition"
+            href={link}
+          >
+            詳細はこちら
+          </a>
+        )}
       </div>
     </div>
   );
